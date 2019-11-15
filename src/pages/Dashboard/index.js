85e4cc3ddda66e1347b9984b4cc2c55709c70c51
Luiz Elias <luizeliasceem@gmail.com>
2019-11-15 17:50:41 -0300

    start socketio connection on dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import socketio from 'socket.io-client';
 import api from '../../services/api';
 import './styles.css';
 
 export default function Dashboard() {
   const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    const user_id = localStorage.getItem('user');
+    const socket = socketio('http://localhost:3333', {
+      query: {
+        user_id,
+      },
+    });
+  }, []);
+
   useEffect(() => {
     async function loadSpots() {
       const user_id = localStorage.getItem('user');
